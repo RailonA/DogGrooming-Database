8c0921207e44dc2792676b6991fe5eb0c7595c85
RailonA <railonacosta@gmail.com>
2021-09-20 16:27:10 -0400

    Add robucop

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ServicesController < ApplicationController
   def index
     services = Service.all
-    render json: { services: services }, status: 200
+    render json: services
   end
+
+
 end

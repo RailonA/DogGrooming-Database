fec37a2cb873882bb4091f8fe151ad889162df8b
RailonA <railonacosta@gmail.com>
2021-11-12 15:15:23 -0500

    Make change

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::AppointmentsController < ApplicationController
   before_action :authenticate, only: [:create]
 
+  def index
+    @appointments = Appointment.all
+    render json: @appointments
+  end
+
   def create
     @appointment = Appointment.create!(appointment_params)
     render json: @appointment
